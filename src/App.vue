42bc47ca62b2752e6ce99c6013508a6108b4c8b4
Dalibor Mitić <35170405+dalibormitic@users.noreply.github.com>
2020-03-13 21:10:02 +0100

    Added App.vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <forma></forma>
+  </div>
+</template>
+
+<script>
+import Forma from "./components/Forma";
+import Vue from "vue";
+import { BootstrapVue } from "bootstrap-vue";
+Vue.use(BootstrapVue);
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+export default {
+  components: {
+    forma: Forma
+  }
+};
+</script>
+
+<style>
+</style>
